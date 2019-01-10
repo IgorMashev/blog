62d626287df2f5395843eb1edec3f0b104cd8070
i.mashevskyi@internetdevels.com <i.mashevskyi@internetdevels.com>
2019-01-10 14:38:36 +0200

    Created plugin generator

--- web/modules/custom/blog_hero/src/Generator/BlogHeroPluginGenerator.php
+++ web/modules/custom/blog_hero/src/Generator/BlogHeroPluginGenerator.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\blog_hero\Generator;
 
+use Drupal\Core\Entity\ContentEntityType;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use DrupalCodeGenerator\Command\BaseGenerator;
 use DrupalCodeGenerator\Utils;
@@ -12,17 +13,32 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class BlogHeroPluginGenerator extends BaseGenerator {
 
+  /**
+   * {@inheritdoc}
+   */
   protected $name = 'plugin-blog-hero';
 
+  /**
+   * {@inheritdoc}
+   */
   protected $alias = 'dh';
 
+  /**
+   * {@inheritdoc}
+   */
   protected $description = 'Generates BlogHero plugin.';
 
+  /**
+   * {@inheritdoc}
+   */
   protected $templatePath = __DIR__ . '/templates';
 
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
   protected $entityTypeManager;
 
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, string $name = NULL) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, $name = NULL) {
     parent::__construct($name);
 
     $this->entityTypeManager = $entity_type_manager;
@@ -41,6 +57,35 @@ class BlogHeroPluginGenerator extends BaseGenerator {
     $questions['is_video'] = new ConfirmationQuestion(t('Do you want to specify video?'), FALSE);
 
     $vars = &$this->collectVars($input, $output, $questions);
+    $vars['name'] = Utils::camelize($vars['plugin_id']);
+    $vars['type'] = Utils::camelize($vars['blog_hero_plugin_type']);
+    $vars['twig_template'] = 'blog-hero-' . $vars['blog_hero_plugin_type'] . '-plugin.html.twig';
+
+    //Additional questions.
+    $questions = [];
+
+    if ($vars['blog_hero_plugin_type'] == 'path') {
+      $questions['match_type'] = new ChoiceQuestion(t('Match type for path'), [
+        'listed' => t('Only on listed page'),
+        'unlisted' => t('All except listed'),
+      ], 'listed');
+    }
+
+    if ($vars['blog_hero_plugin_type'] == 'entity') {
+      $entity_types = [];
+      foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
+        if ($entity_type instanceof ContentEntityType) {
+          $entity_types[$entity_type_id] = $entity_type->getLabel();
+        }
+      }
+      $questions['entity_type'] = new ChoiceQuestion(t('Entity type'), $entity_types);
+    }
+
+    $vars = &$this->collectVars($input, $output, $questions, $vars);
+
+    $this->addFile()
+      ->path('src/Plugin/BlogHero/{type}/{name}.php')
+      ->template($vars['twig_template']);
   }
 
   public function askForPluginType(&$question) {
