498686c4b8fcd61b2aa6b4b7c38d72e9014fb181
i.mashevskyi@internetdevels.com <i.mashevskyi@internetdevels.com>
2019-02-02 13:14:08 +0200

    Saturday changes.

--- web/themes/custom/blogger/assets/js/paragraph-code.js
+++ web/themes/custom/blogger/assets/js/paragraph-code.js
@@ -2,8 +2,9 @@
 
     Drupal.behaviors.paragraphCodeHighlight = {
         attach: function (context, settings) {
-            const elements = $(context).find('pre code').once('highlight');
+            const elements = $(context).find('pre code');
 
+            console.log(elements);
             if (elements.length) {
                 $.each(elements, (key, element) => {
                     hljs.highlightBlock(element);
